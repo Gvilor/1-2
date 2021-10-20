805a81529ce714226e6724931235132ea7c4cf4b
Gvilor <gvilor@yandex.ru>
2021-10-20 14:29:54 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+
+
+
+            Console.WriteLine(Logic.Multiply(x, y));
+
+        }
+    }
+
+    public class Logic
+    {
+        public static int Multiply(int x, int y)
+        {
+            int b = 0;
+
+            for (int i = 0; i < y; i++)
+            {
+                b = b + x;
+            }
+            return b;
+        }
+    }
+}
